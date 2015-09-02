98055588ca8708c1f84e9f68cafa07df3c7e89db
SAM <samvbeckmann@gmail.com>
2015-09-01 21:51:37 -0500

    Cleaned up import statements.

--- src/main/java/com/qkninja/network/thirdparty/waila/WailaTransporterHandler.java
+++ src/main/java/com/qkninja/network/thirdparty/waila/WailaTransporterHandler.java
@@ -13,9 +13,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
-import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.FluidTankInfo;
 
 import java.util.List;
 
