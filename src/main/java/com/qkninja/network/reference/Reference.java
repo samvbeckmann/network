32712f7ccc212bf87960f5e4e108e032a29ec04f
SAM <samvbeckmann@gmail.com>
2015-12-14 13:23:19 -0600

    Updated version to 0.3.0

--- src/main/java/com/qkninja/network/reference/Reference.java
+++ src/main/java/com/qkninja/network/reference/Reference.java
@@ -9,7 +9,7 @@ public class Reference
 {
     public static final String MOD_ID = "Network";
     public static final String MOD_NAME = "Network";
-    public static final String VERSION = "1.7.10-0.2.1";
+    public static final String VERSION = "1.7.10-0.3.0";
     public static final String SERVER_PROXY_CLASS = "com.qkninja.network.proxy.ServerProxy";
     public static final String CLIENT_PROXY_CLASS = "com.qkninja.network.proxy.ClientProxy";
     public static final String GUI_FACTORY_CLASS = "com.qkninja.network.client.gui.GuiFactory";
